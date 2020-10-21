13879a3c83d30a0c21ce621cf119ca23add73f48
lromanova <perfectzvekla@gmail.com>
2020-10-21 20:10:44 +0300

    изменен метод update

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -17,7 +17,14 @@ class WallService {
     fun update (postToUpdate: Post): Boolean{
         for ((index,post) in posts.withIndex()){
             if(post.id == postToUpdate.id){
-               posts[index] = postToUpdate
+               posts[index] = post.copy(id= postToUpdate.id, ownerId = postToUpdate.ownerId,fromId = postToUpdate.fromId,
+                       createdBy = postToUpdate.createdBy, date = postToUpdate.date,text = postToUpdate.text,
+                       replyOwnerId = postToUpdate.replyOwnerId,replyPostId = postToUpdate.replyPostId,
+                       friendsOnly = postToUpdate.friendsOnly,comments = postToUpdate.comments,copyright = postToUpdate.copyright,
+                       likes = postToUpdate.likes,reposts = postToUpdate.reposts,views = postToUpdate.views,
+                       postType = postToUpdate.postType,signerId = postToUpdate.signerId,canPin = postToUpdate.canPin,
+                       canDelete = postToUpdate.canDelete,canEdit = postToUpdate.canEdit,isPinned = postToUpdate.isPinned,
+                       markedAsAds = postToUpdate.markedAsAds,isFavorite = postToUpdate.isFavorite,postponedId = postToUpdate.postponedId)
                 return true
             }
         }
